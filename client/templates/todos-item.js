d08d1a74c558f92e5082b81960899e2bf2c9426d
Brandon Borders <brandon@camelcased.com>
2015-03-10 20:23:54 -0700

    Add event handler for on blur

--- client/templates/todos-item.js
+++ client/templates/todos-item.js
@@ -1,14 +1,25 @@
 Template.todosItem.events({
-  'submit form': function(e) {
+  'submit form, blur input[type=text]': function(e) {
       e.preventDefault();
 
-      var val = e.target.querySelector("input[type=text]").value;
-      if (val.toLowerCase() === this.definition) {
-          e.target.querySelector("input[type=checkbox]").setAttribute("checked", true);
-          e.target.querySelector("input[type=checkbox]").classList.remove("incorrect");
+      if (e.target.nodeName === "INPUT") {
+          var target = e.target.parentElement;
       } else {
-          e.target.querySelector("input[type=checkbox]").setAttribute("checked", true);
-          e.target.querySelector("input[type=checkbox]").classList.add("incorrect");
+          var target = e.target;
+      }
+
+      var val = target.querySelector("input[type=text]").value;
+      if (val) {
+          if (val.toLowerCase() === this.definition) {
+              target.querySelector("input[type=checkbox]").checked = true;
+              target.querySelector("input[type=checkbox]").classList.remove("incorrect");
+          } else {
+              target.querySelector("input[type=checkbox]").checked = true;
+              target.querySelector("input[type=checkbox]").classList.add("incorrect");
+          }
+      } else {
+          target.querySelector("input[type=checkbox]").checked = false;
+          target.querySelector("input[type=checkbox]").classList.remove("incorrect");
       }
   }
 });
